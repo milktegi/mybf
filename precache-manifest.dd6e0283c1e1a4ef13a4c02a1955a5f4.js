98e6ceebbd08f11ee3336372d786c240e086cbd9
milktegi <milkteegi@gmail.com>
2019-01-17 10:25:06 +0900

    Updates

---
+++ precache-manifest.dd6e0283c1e1a4ef13a4c02a1955a5f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/mybf/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "65fa85a1772c067c8648",
+    "url": "/mybf/static/js/runtime~main.65fa85a1.js"
+  },
+  {
+    "revision": "7fac5acdfb97418b1e6a",
+    "url": "/mybf/static/js/main.7fac5acd.chunk.js"
+  },
+  {
+    "revision": "785317920b0333613437",
+    "url": "/mybf/static/js/1.78531792.chunk.js"
+  },
+  {
+    "revision": "7fac5acdfb97418b1e6a",
+    "url": "/mybf/static/css/main.a3e3ed14.chunk.css"
+  },
+  {
+    "revision": "785317920b0333613437",
+    "url": "/mybf/static/css/1.8769ea31.chunk.css"
+  },
+  {
+    "revision": "a3559fe9677e3bf6b2b965dafa0a3152",
+    "url": "/mybf/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b964958c7cdb7c5b86ff7e62a308074.js"
+  "/mybf/precache-manifest.dd6e0283c1e1a4ef13a4c02a1955a5f4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/mybf/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
